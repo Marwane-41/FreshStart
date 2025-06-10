a589d272dd03be05eea8699437f80f4c408175b3
Marwane Zaoudi <zaoudimarwane5@gmail.com>
2025-06-09 23:14:37 -0600

    fixes

---
+++ business_template1/pages/index.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div className="text-center text-2xl">
+      <h1>Welcome to my first business Template
+      </h1>
+
+
+    </div>
+  );
+}
